1072fb729eda4a5460ccfdbc7ac46e0b46bb7679
alisutcu <alisutchu@gmail.com>
2022-01-02 21:26:26 +0300

    UI MODS - Login/register screen's done from scratch - Navigation controllers refactored

--- guntagram/viewcontrollers/LoginViewController.swift
+++ guntagram/viewcontrollers/LoginViewController.swift
@@ -21,16 +21,6 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func loginButtonPressed(_ sender: Any) {
         if let password = passwordTextField.text, let email = emailTextField.text {
             self.userDataSource.logInUser(email: email, password: password)
